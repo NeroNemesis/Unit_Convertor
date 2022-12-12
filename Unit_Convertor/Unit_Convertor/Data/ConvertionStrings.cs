716e467672b2a51c27ac59ae153b61bb6c2f3c66
NeroNemesis <72110273+NeroNemesis@users.noreply.github.com>
2022-12-12 01:12:23 -0500

    First steps

---
+++ Unit_Convertor/Unit_Convertor/Data/ConvertionStrings.cs
@@ -0,0 +1,33 @@
+﻿namespace Unit_Convertor.Data
+{
+    public class ConvertionStrings
+    {
+        public string Dist_unit {get;set;} = String.Empty;
+        public string Km { get; set; } = String.Empty;
+        public string M = String.Empty;
+        public string Dm = String.Empty;
+        public string Cm = String.Empty;
+        public string Mm = String.Empty;
+        public string Mi = String.Empty;
+        public string Inch = String.Empty;
+        public string Ft = String.Empty;
+        public string Yd = String.Empty;
+
+        //weight
+        public string Weight_unit = String.Empty;
+        public string Kg = String.Empty;
+        public string G = String.Empty;
+        public string Dg = String.Empty;
+        public string Cg = String.Empty;
+        public string Mg = String.Empty;
+        public string St = String.Empty;
+        public string Lb = String.Empty;
+        public string Ounce = String.Empty;
+
+        //temperatures
+        public string Temp_unit = String.Empty;
+        public string Cels = String.Empty;
+        public string Fahren = String.Empty;
+        public string Kelv = String.Empty;
+    }
+}
